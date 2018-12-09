47be50c13b28e2adcb5d8623e6a3a510ef058171
cameo-js <kjs8469@gmail.com>
2018-12-09 23:54:24 +0900

    WIP

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import { Row } from './Row';
-import { data } from './data';
 
 const KEY = "entities";
 
